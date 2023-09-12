c16902df02fc0c9dd66666ce45d9785bff7c6981
youabledev <youable.framios@gmail.com>
2023-09-13 00:37:32 +0900

    add: project

---
+++ demystify-swiftui/demystify-swiftui/demystify_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  demystify_swiftuiApp.swift
+//  demystify-swiftui
+//
+//  Created by zumin you on 2023/09/13.
+//
+
+import SwiftUI
+
+@main
+struct demystify_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
